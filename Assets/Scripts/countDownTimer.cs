d321870fb71daa9b4d023cdd3128ff08e1e4658b
funkybzh <MickMac@macbook-pro-de-mickmac-1.home>
2016-07-12 23:59:37 +0200

    add newprojectmaze

---
+++ Assets/Scripts/countDownTimer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class countDownTimer : MonoBehaviour {
+	float timeRemaining = 65;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeRemaining -= Time.deltaTime;
+		if((timeRemaining < 4 && timeRemaining > 3.98f) || (timeRemaining < 3 && timeRemaining > 2.98f) || (timeRemaining < 2 && timeRemaining > 1.98f))
+		{
+			GetComponent<AudioSource>().PlayOneShot(countAudio);
+		}﻿
+	}
+
+	void OnGUI(){
+		if (timeRemaining > 0) {
+			GUI.Label (new Rect (100, 100, 200, 100), "Time Remaining : " + timeRemaining);
+		} else {
+			GUI.Label (new Rect (100, 100, 100, 100), "NOOB");
+		}
+	}
+}
